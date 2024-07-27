bdb79e7de281a665142bea51649f74fb18127b5a
Vikas Bishnoi <114574074+Vikky04@users.noreply.github.com>
2024-07-27 14:47:10 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,10 @@
+const sum= (a,b)=>a+b;
+const mult= (a,b)=>a*b;
+const g=9.8;
+// modeule.exports.g=9.8;
+module.exports=123;
+module.exports={sum:sum,
+    g:g
+}
+// this is a object that contains which are want to export
+// module.exports="Hello";
---
+++ script.js
@@ -0,0 +1,17 @@
+const someval=require("./math");
+
+console.log(someval);
+
+/*
+if we want to some data exported use then use require
+on that file and exported file write as module.exports={something}
+PS C:\Users\vicky\OneDrive\main\WEBDEV> node script.js
+123
+PS C:\Users\vicky\OneDrive\main\WEBDEV> node script.js
+123
+123
+PS C:\Users\vicky\OneDrive\main\WEBDEV> node script.js
+{ sum: [Function: sum], g: 9.8 }
+PS C:\Users\vicky\OneDrive\main\WEBDEV>
+
+*/
