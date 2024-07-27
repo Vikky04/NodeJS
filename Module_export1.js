32fd75e2b7203346aa4c4a81248ae87590c9053b
Vikas Bishnoi <114574074+Vikky04@users.noreply.github.com>
2024-07-27 14:59:23 +0530

    Update Module_export1.js

--- Module_export1.js
+++ Module_export1.js
@@ -5,6 +5,11 @@ console.log(someval);
 /*
 if we want to some data exported use then use require
 on that file and exported file write as module.exports={something}
+
+if we want to export data from other directory :
+1. All exoprted file combine at index.js(must) and then exports those from index.js
+2.When you want to require data in other directory simply require("directory name") this check in directory foe index.js file always.
+
 PS C:\Users\vicky\OneDrive\main\WEBDEV> node script.js
 123
 PS C:\Users\vicky\OneDrive\main\WEBDEV> node script.js
